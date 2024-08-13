2951e996b5424de56b1cbc82ef6b8bd4ca9ea590
Girishmasade <girishmasade@gmail.com>
2024-08-13 18:31:40 +0530

    database config

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,6 @@
+import {connect} from '@/database/dbconfig'
+import User from '@/models/userModel'
+import { NextResponse, NextRequest } from 'next/server'
+// import bcryptjs from bcryptjs;
+
+connect();
--- src/database/dbconfig.ts
+++ src/database/dbconfig.ts
@@ -6,9 +6,15 @@ export const connect = async() =>{
         const connection = mongoose.connection;
 
         connection.on('connected', ()=>{
+            console.log('Mongodb connected successfully');
+        })
 
+        connection.on('error', () => {
+            console.log('Mongodb connection error');
+            process.exit
         })
     } catch (error) {
+        console.log('something went wrong', error);
         
     }
 }
